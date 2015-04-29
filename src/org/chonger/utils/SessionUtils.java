da244b25c89da77357afb2858292696f999ef1e9
Daniel <daniel-yim@live.com>
2015-04-29 10:05:46 +0800

    新增牛场访问

--- src/org/chonger/utils/SessionUtils.java
+++ src/org/chonger/utils/SessionUtils.java
@@ -1,6 +1,7 @@
 package org.chonger.utils;
 
 import org.apache.struts2.ServletActionContext;
+import org.chonger.entity.jbxx.NCJBXX;
 import org.chonger.entity.system.User;
 import org.chonger.service.system.UserManager;
 
@@ -13,4 +14,19 @@ public class SessionUtils {
 		Object sessionObject=ServletActionContext.getRequest().getSession().getAttribute(UserManager.USERSESSIONKEY);	
 		return sessionObject==null?null:(User)sessionObject;
 	}
+	
+	/**
+	 * 获取当前用户的牛场信息
+	 */
+	public static NCJBXX getNcxx()
+	{
+		User user=getUser();
+		if(user!=null)
+		{
+			NCJBXX ncxx=user.getNcjbxx();
+			return ncxx==null?null:ncxx;
+		}
+		return null;
+	}
+	
 }
