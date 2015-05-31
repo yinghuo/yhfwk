d16abd6e4341d1e0b7890cdee551f1fe1ff9b549
Daniel <daniel-yim@live.com>
2015-05-31 21:57:57 +0800

    娣诲姞鐗涘彧绫诲埆浠诲姟

---
+++ src/org/chonger/service/task/NzlbTask.java
@@ -0,0 +1,13 @@
+package org.chonger.service.task;
+
+import java.util.TimerTask;
+
+public class NzlbTask extends TimerTask {
+
+	@Override
+	public void run() {
+		// Note Auto-generated method stub
+
+	}
+
+}
